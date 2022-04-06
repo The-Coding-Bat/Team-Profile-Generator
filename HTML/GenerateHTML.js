62dcadef3c20ca1b8ce3042f6e7323ab773f31a9
Ted J <tedmjablonowski@gmail.com>
2022-04-06 12:49:10 -0400

    Quick HTML fix

--- HTML/GenerateHTML.js
+++ HTML/GenerateHTML.js
@@ -29,18 +29,19 @@ const generateTeamProfiles = function (teamProfiles) {
     return`
     <!DOCTYPE html>
     <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Team Profile</title>
-        <link rel=stylesheet></link>
-    </head>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Team Profile</title>
+            <link rel=stylesheet></link>
+        </head>
 
-    <body>
-        <header id="team-header></header>
+        <body>
+            <header id="team-header></header>
 
-        <main id="employee-cards"> </main>
-    </body>
+            <main id="employee-cards"> </main>
+        </body>
+    </html>
     `
 }
 
